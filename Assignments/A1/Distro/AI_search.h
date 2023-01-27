8f9c6b304bffedc6d82a2b887b1bb740e11fab60
Richard Ye <33409792+richardye101@users.noreply.github.com>
2023-01-26 23:02:00 -0500

    Test/traversal logic (#3)
    * traversal logic and update git ignore
    
    * Remove .vscode dir
    
    * WIP
    
    * Update APIs
    
    * WIP - compiles & passes tests
    
    * Work on search function more
    
    * Make some debugging progress
    
    * Fix helper function
    
    * Things technically working - but looping on DFS
    
    Co-authored-by: Sam Prokopchuk <ssprokopchuk@gmail.com>

--- Assignments/A1/Distro/AI_search.h
+++ Assignments/A1/Distro/AI_search.h
@@ -25,10 +25,30 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 #include "board_layout.h"
 
+// BEGIN MODES
+#define MODE_BFS 0
+#define MODE_DFS 1
+#define MODE_A_STAR 2
+// END MODES
+
+// BEGIN DIRECTIONS
+#define DIRECTION_UP 0
+#define DIRECTION_RIGHT 1
+#define DIRECTION_DOWN 2
+#define DIRECTION_LEFT 3
+// END DIRECTIONS
+
 // BEGIN STRUCT DEFS
+typedef int (*HEURISTIC_FUNCTION)(int x, int y, int cat_loc[10][2],
+                                  int cheese_loc[10][2], int mouse_loc[1][2],
+                                  int cats, int cheeses,
+                                  double gr[graph_size][4]);
+
 // Deque will be used for both queue and stack
 typedef struct Cord {
   int x, y;
@@ -54,27 +74,45 @@ typedef struct MinHeap {
   HeapItem data[graph_size];
   int size;
 } MinHeap;
+
+typedef struct DataStructure {
+  Deque *deque;
+  MinHeap *min_heap;
+  int mode;
+  HEURISTIC_FUNCTION heuristic;
+} DataStructure;
 // END STRUCT DEFS
 
 // BEGIN STRUCT HELPER FUNCTION PROTOS
-DequeItem *DequeItem_new(int x, int y);
+DequeItem *DequeItem_new(Cord cord);
 Deque *Deque_new(void);
 MinHeap *Heap_new(void);
-void Deque_push_front(Deque *deque, int x, int y);
-void Deque_push_back(Deque *deque, int x, int y);
+void Deque_push_front(Deque *deque, Cord cord);
+void Deque_push_back(Deque *deque, Cord cord);
 Cord Deque_pop_front(Deque *deque); 
 Cord Deque_pop_back(Deque* deque);
 MinHeap* MinHeap_new(void);
-void MinHeap_insert(MinHeap* min_heap, int x, int y, int priority);
+void MinHeap_insert(MinHeap *min_heap, Cord cord, int priority);
 Cord MinHeap_pop(MinHeap *min_heap);
+DataStructure *DataStructure_new(int mode, HEURISTIC_FUNCTION heuristic);
+void DataStructure_insert(DataStructure *data_structure, Cord cord,
+                          int priority);
+Cord DataStructure_pop(DataStructure *data_structure);
+int DataStructure_size(DataStructure* data_structure);
+void DataStructure_dtor(DataStructure* data_structure);
 // END STRUCT HELPER FUNCTION PROTOS
 
 // Function prototypes for D84 - Unit 1 - Search assignment solution
 
 // BEGIN MISC HELPER FUNCTION PROTOS
-void index_to_cord(int index, int *x, int *y);
-int cord_to_index(int x, int y);
-int is_cord_valid(int x, int y);
+Cord get_next_cord(Cord cord, int direction);
+Cord index_to_cord(int index);
+int cord_to_index(Cord cord);
+int is_index_valid(int index);
+int is_cord_valid(Cord cord);
+int equal_cords(Cord a, Cord b);
+void construct_path(int path[graph_size][2], int came_from[graph_size],
+                    const Cord start, const Cord goal);
 // END MISC HELPER FUNCTION PROTOS
 
 
--- Assignments/A1/Distro/temp_search_algo.c
+++ Assignments/A1/Distro/temp_search_algo.c
@@ -1,4 +1,5 @@
 // insert the start node
+#include "AI_search.h"
 insert(data_structure, start);
 /*
 Since the traversal and checking of cheese/cats/walls are all the same
@@ -9,9 +10,9 @@ of code written and is easy to read and debug.
 */
 
 // array to store where nodes came from, to create a path
-int cameFrom[graph_size];
+int came_from[graph_size];
 // Create an array to keep track of queued nodes
-bool visited[n];
+bool visited[graph_size];
 memset(visited, false, sizeof(visited));
 visited[start] = true;
 
@@ -25,51 +26,34 @@ bool found == false;
 while (!isEmpty(data_structure) && !found) {
   // keep track of the current location
   int current = extract(data_structure);
-  int cur_x = current % size_X;
-  int cur_y = current / size_Y;
   // need to check if pixel is one of cheese_loc[10][2]
   for (int i = 0; i < cheeses; i++) {
     int cheese_x = cheese_loc[i][0];
     int cheese_y = cheese_loc[i][1];
     if (current == (cheese_x + (cheese_y * size_X))) {
-      path = constructPath(cameFrom, cheese_x + (cheese_y * size_X), start);
+      path = construct_path(path, cameFrom, cheese_x + (cheese_y * size_X), start);
       printf("Reached a cheese piece at %d, %d\n", cheese_x, cheese_y);
       found = true;
       break;
     }
   }
+  
   // insert all unvisited neighbors of the current node
-  for (int i = 0; i < 4; i++) {
-    int x, y;
-    switch (i) {
-    case 0:
-      x = cur_x;
-      y = cur_y - 1;
-      break;
-    case 1:
-      x = cur_x + 1;
-      y = cur_y;
-      break;
-    case 2:
-      x = cur_x;
-      y = cur_y + 1;
-      break;
-    case 3:
-      x = cur_x - 1;
-      y = cur_y;
-      break;
+  for (int direction=0; direction<4; ++direction) {
+    const Cord cord = next_cord((Cord){cur_x, cur_y}, direction);
+    const int index = 
+    if (!is_cord_valid(cord)) {
+      continue;
     }
-    // check dimensions are in bounds
-    if (x < 32 && x >= 0 && y >= 0 && y < 32) {
-      int idx = x + (y * size_X);
-      // check for cats
-      bool is_cat = false;
-      for (int i = 0; i < cats; i++) {
-        if (idx == (cat_loc[i][0] + (cat_loc[i][1] * size_X))) {
-          is_cat = true;
-          break;
-        }
+    bool is_cat = false;
+    for (int cat = 0; cat < cats; ++cat) {
+      if (is_cord_equal(cord, (Cord){cat_loc[i][0], cat_loc[i][1]})) {
+        is_cat = true;
+        break;
       }
+    }
+    if (!is_visited[cord_to_index()
+  }
       // queue if neighbour is not visited, no wall (weight != 0), no cat
       if (!visited[idx] && gr[current][i] && !is_cat) {
         if (mode == 2) {
@@ -86,4 +70,32 @@ while (!isEmpty(data_structure) && !found) {
   }
   visit_order[x][y] = order;
   order++;
+}
+
+/*
+THe function to construct a path
+*/
+
+int [][] constructPath(int* cameFrom, int goal, int start)
+{
+    int current = goal;
+    int reverse_path[graph_size];
+	int path[graph_size][2];
+    int pathIndex = 0;
+    while (current != start)
+    {
+        reverse_path[pathIndex] = current;
+        current = cameFrom[current];
+		pathIndex++;
+    }
+    reverse_path[pathIndex] = start;
+	for(i = pathIndex; i > -1; i--){
+		path[i][0] = reverse_path[i] % size_X;
+		path[i][1] = reverse_path[i] / size_Y;
+	}
+	return path;
+    // printf("The path from start to goal is: ");
+    // for (int i = pathIndex - 1; i >= 0; i--)
+    //     printf("%d ", path[i]);
+    // printf("\n");
 }
