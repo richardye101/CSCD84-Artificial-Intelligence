afff2c701a17eb53fc0b4fd837067085c784f11d
Sam Prokopchuk <36868899+SamProkopchuk@users.noreply.github.com>
2023-04-12 21:57:47 -0400

    Sam A4 Part 1 (#19)
    * MAke le sigmoidsoidf funciton
    
    * Atutoformat
    
    * Write somes lines of code
    
    * Clean up feedforward_1layer
    
    * FIX
    
    * Autoformat
    
    * Add outline of 2layer backprop
    
    * 2layer backprop improvements
    
    * Fix compilation errors
    
    * Minor changes, still 2layer doesn't work
    
    * Minor changes, still 2layer doesn't work
    
    * LKAJDKLASJDLKASJDKALSDJ

--- Assignments/A4/Distro/NeuralNets.h
+++ Assignments/A4/Distro/NeuralNets.h
@@ -35,9 +35,7 @@
 // Network learning rate
 #define ALPHA .01
 // Scaling factor for sigmoid function input <--- MIND THIS!
-#define SIGMOID_SCALE .01
-
-#define BIG_DBL 100000000000.0
+#define SIGMOID_SCALE 0.01
 
 // Function prototypes for D84 - Unit 4 - Neural Nets
 int train_1layer_net(double sample[INPUTS], int label,
@@ -78,11 +76,18 @@ double activation_prime(double output, double (*sigmoid)(double input));
 
 // Compute the dot product between matrix A and vector x.
 // Note that matrix A is column-major.
-void dot_product(double A[][], double x[], double b[], int rows, int cols);
+void dot_product(double *A, double x[], double b[], int rows, int cols,
+                 int memory_cols);
 
 // Set each array element to the activation function applied to it.
-void apply_activation_function(double array[], int size, double (*sigmoid)(double input)) {
+void apply_activation_function(double array[], int size,
+                               double (*sigmoid)(double input),
+                               double scaling_factor);
 
 // If you need to add any function prototypes yourself, you can do so *below*
 // this line.
+int argmax(double array[], int size);
+
+double target_output(int i, int label, double (*sigmoid)(double input));
+
 #endif
